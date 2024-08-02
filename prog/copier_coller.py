741afcdfd0b162575454a1f06830fa088ce91687
These-SCAI2023 <caroline.parfait@sorbonne-universite.fr>
2024-08-02 14:34:19 +0200

    maj small-ELTeC-fra

--- prog/copier_coller.py
+++ prog/copier_coller.py
@@ -1,15 +1,20 @@
 import glob
 import json
 import shutil
+import os
 
-path_input="../copier_coller/DATA_ELTeC-eng_spaCy3.5.1_sim2-3-word/*/*OCR/*/SIM"
+path_input="../small-ELTeC-fra/*/*REF/*.txt"
 
 
 for path in glob.glob(path_input):
-    # print(path)
-    path_output=path.split("/")
-    path_output="/".join([path_output[0],path_output[1],"DATA_ELTeC-eng_spaCy3.5.1",path_output[3],path_output[4],path_output[5],path_output[6]])
-    print(path_output)
+    if "PP" in path:
+        print(path)
+        os.remove(path)
+    # path_output=path.split("/")
+    # path_output="/".join([path_output[0],path_output[1],"DATA_ELTeC-eng_spaCy3.5.1",path_output[3],path_output[4],path_output[5],path_output[6]])
+    # print(path_output)
 
+    #
+    # shutil.copytree(path,path_output)
 
-    shutil.copytree(path,path_output)
+    # shutil.rmtree(path)
