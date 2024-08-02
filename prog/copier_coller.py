0b5211afd9aa833f8d4269662123c64373ed783b
These-SCAI2023 <caroline.parfait@sorbonne-universite.fr>
2024-08-02 20:17:59 +0200

    maj small-ELTeC-fra & small-ELTeC-fra2024 & small-ELTeC-fra-2021-2024

--- prog/copier_coller.py
+++ prog/copier_coller.py
@@ -3,18 +3,22 @@ import json
 import shutil
 import os
 
-path_input="../small-ELTeC-fra/*/*REF/*.txt"
+path_input="../small-ELTeC-fr*/*/*OCR/*/"
 
 
 for path in glob.glob(path_input):
-    if "PP" in path:
+    if "jspll" in path or "jspl" in path:
         print(path)
-        os.remove(path)
+        shutil.rmtree(path)
+
+    # path_rename= "/".join(path.split("/")[:3])+"/"+path.split("/")[2]+"_REF"
+    # print(path_rename)
+    # os.rename(path, path_rename)
+
     # path_output=path.split("/")
     # path_output="/".join([path_output[0],path_output[1],"DATA_ELTeC-eng_spaCy3.5.1",path_output[3],path_output[4],path_output[5],path_output[6]])
     # print(path_output)
 
-    #
+    # os.remove(path)
     # shutil.copytree(path,path_output)
 
-    # shutil.rmtree(path)
