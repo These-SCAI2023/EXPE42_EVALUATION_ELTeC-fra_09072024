81466d5de8c10b954ffd2248da8549b3549dbe2e
These-SCAI2023 <caroline.parfait@sorbonne-universite.fr>
2024-08-02 21:30:32 +0200

    prog

--- prog/copier_coller.py
+++ prog/copier_coller.py
@@ -3,17 +3,27 @@ import json
 import shutil
 import os
 
-path_input="../small-ELTeC-fr*/*/*OCR/*/"
+# path_input="../ARCHEO_Corr/small-ELTeC-fra2024*/*"
+path_input="../ARCHEO_Corr/small-ELTeC-fra2024*/*/*REF"
 
 
 for path in glob.glob(path_input):
-    if "jspll" in path or "jspl" in path:
-        print(path)
-        shutil.rmtree(path)
+    # print(path)
+    # if "AIMARD" not in path:
+        # print(path)
+        # path_rename=path.split("_")
+        # path_rename = "_".join(path_rename[:3])
 
-    # path_rename= "/".join(path.split("/")[:3])+"/"+path.split("/")[2]+"_REF"
-    # print(path_rename)
-    # os.rename(path, path_rename)
+
+    # if "jspll" in path or "jspl" in path:
+    #     print(path)
+        # shutil.rmtree(path)
+
+    path_rename= "/".join(path.split("/")[:4])+"/"+path.split("/")[3]+"_REF"
+    print(path_rename)
+    os.rename(path, path_rename)
+
+
 
     # path_output=path.split("/")
     # path_output="/".join([path_output[0],path_output[1],"DATA_ELTeC-eng_spaCy3.5.1",path_output[3],path_output[4],path_output[5],path_output[6]])
