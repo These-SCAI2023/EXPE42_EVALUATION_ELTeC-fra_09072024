49f49d27f72729060d5ac37cda98fc0a2b57d364
These-SCAI2023 <caroline.parfait@sorbonne-universite.fr>
2024-09-16 11:36:06 +0200

    small-Ecoproject & autres nouveautés

--- prog/EN_repere.py
+++ prog/EN_repere.py
@@ -2,59 +2,60 @@ import json
 import glob
 
 
-def lire_json (chemin):
+def lire_json(chemin):
     with open(chemin) as mon_fichier:
-        data = json.load(mon_fichier)
-    return data
+        datajson = json.load(mon_fichier)
+    return datajson
 
-def NERtools(chemin):
-    infos=chemin.split('/')[-1]
-    tools=infos.split('_')[-1]
-    version=infos.split('.')[0]
+
+def nertools(chemin):
+    infos = chemin.split('/')[-1]
+    tools = infos.split('_')[-1]
+    version = infos.split('.')[0]
     return version, tools
 
-path_SEM="../small-ELTeC-fra_SEM-WiNER/ADAM/*/*/*"
-path_CasEN="../small-ELTeC-fra_CasEN/ADAM/*/*/*"
-path_NERtools = "../small-ELTeC-fra-2021-2024_REN/ADAM/*"
+
+path_SEM = "../small-ELTeC-fra_SEM-WiNER/ADAM/*/*/*"
+path_CasEN = "../small-ELTeC-fra_CasEN/ADAM/*/*/*"
+path_NERtools = "../small-ELTeC-fra-2021-2024_REN/NOAILLES/*"
 
 
 EN_input = input("Entité recherchée : ")
 for pathNERtools in glob.glob(path_NERtools):
     if "REF" in pathNERtools:
         # print(pathNERtools)
-        pathNERtools_REF= f"{pathNERtools}/NER"
+        pathNERtools_REF = f"{pathNERtools}/NER"
         # print(pathNERtools_REF)
         for path in glob.glob(f"{pathNERtools_REF}/*.json"):
-            data=lire_json(path)
-            if type(data) == dict :
-                toolsNER = NERtools(path)
+            data = lire_json(path)
+            if type(data) is dict:
+                toolsNER = nertools(path)
                 # print(toolsNER)
                 # print(data)
                 for key, value in data.items():
                     # print(value)
                     for k, v in value.items():
-                        if k =="text":
+                        if k == "text":
                             # print(v)
-                            if EN_input in v :
-                                print(toolsNER, value["label"],value["jalons"], v)
-    # if "OCR" in pathNERtools:
-    #     # print(pathNERtools)
-    #     pathNERtools_REF= f"{pathNERtools}/*/NER"
-    #     # print(pathNERtools_REF)
-    #     for path in glob.glob(f"{pathNERtools_REF}/*.json"):
-    #         data=lire_json(path)
-    #         if type(data) == dict :
-    #             toolsNER = NERtools(path)
-    #             # print(toolsNER)
-    #             # print(data)
-    #             for key, value in data.items():
-    #                 # print(value)
-    #                 for k, v in value.items():
-    #                     if k =="text":
-    #                         # print(v)
-    #                         if EN_input in v :
-    #                             print(toolsNER,value["label"],value["jalons"], v)
-
+                            if EN_input in v:
+                                print(toolsNER, value["label"], value["jalons"], v)
+    if "OCR" in pathNERtools:
+        # print(pathNERtools)
+        pathNERtools_REF = f"{pathNERtools}/*/NER"
+        # print(pathNERtools_REF)
+        for path in glob.glob(f"{pathNERtools_REF}/*.json"):
+            data = lire_json(path)
+            if type(data) is dict:
+                toolsNER = nertools(path)
+                # print(toolsNER)
+                # print(data)
+                for key, value in data.items():
+                    # print(value)
+                    for k, v in value.items():
+                        if k == "text":
+                            # print(v)
+                            if EN_input in v:
+                                print(toolsNER, value["label"], value["jalons"], v)
 # for pathSEM in glob.glob(path_SEM):
 #     print(pathSEM)
 # for pathCasEN in glob.glob(path_CasEN):
--- prog/segmentation_impact.py
+++ prog/segmentation_impact.py
@@ -1,10 +1,28 @@
 import glob
 import spacy
 import json
+
+
 def lire_fichier(chemin):
     f = open(chemin, encoding='utf−8')
     chaine = f.read()
     return chaine
+
+
+def sent_seg(texte_analyse, dico_result, cpt):
+    assert texte_analyse.has_annotation("SENT_START")
+    for sent in texte_analyse.sents:
+        dico_result["segment_" + str(cpt)] = sent.text
+        cpt = cpt + 1
+    return dico_result
+
+
+def tokenisation(texte_analyse, dico_result, cpt):
+    for token in texte_analyse:
+        dico_result[f"token_{cpt}"] = token.text
+        cpt = cpt + 1
+    return dico_result
+
 
 def stocker(chemin, contenu):
     w = open(chemin, "w")
@@ -13,54 +31,52 @@ def stocker(chemin, contenu):
     # print(chemin)
     return chemin
 
-path_corpora="../small-ELTeC-fra-2021-2024_REN/ADAM/*"
-start_char= 16492-200
-end_char= 16505+200
-modeles=["lg","sm","md"]
 
-i=0
-
+path_corpora = "../small-ELTeC-fra-2021-2024_REN/MAUPASSANT/*"
+start_char = 16492-80
+end_char = 16505+80
+modeles = ["lg", "md", "sm"]
+# modeles = ["md"]
 for mod in modeles:
     nlp = spacy.load(f"fr_core_news_{mod}")
-    dico_res = {}
+    dico_sent = {}
+    dico_tok = {}
+    o = 0
+    i = 0
+
     print(f"Le modèle : {mod}")
     for path_file in glob.glob(path_corpora):
         if "REF" in path_file:
-                    ref_files= f"{path_file}/*.txt"
-                    for reffile in glob.glob(ref_files):
-                        print("REF", reffile)
-                        texte = lire_fichier(reffile)
-                        texte = lire_fichier(reffile)
-                        print(texte[start_char:end_char])
-                        doc = nlp(texte[start_char:end_char])
+            ref_files = f"{path_file}/*.txt"
+            for reffile in glob.glob(ref_files):
+                print("REF", reffile)
+                texte = lire_fichier(reffile)
+                # print(texte[start_char:end_char])
+                # doc = nlp(texte[start_char:end_char])
+                doc = nlp(texte)
+                ##Token Segmentation
+                dico_tok = tokenisation(doc, dico_tok, o)
+                ##Sentence Segmentation
+                dico_sent = sent_seg(doc, dico_sent, i)
+                stocker(f"{reffile}_tok_{mod}.json", dico_tok)
+                stocker(f"{reffile}_seg_{mod}.json", dico_sent)
 
-                        for ent in doc.ents:
-                            print(ent.text, ent.start_char, ent.end_char, ent.label_)
+        if "OCR" in path_file:
+            ocr_files = f"{path_file}/*/*.txt"
+            for ocrfile in glob.glob(ocr_files):
+                dicocr_sent = {}
+                dicocr_tok = {}
+                oc = 0
+                ic = 0
+                print("OCR", ocrfile)
+                texte_ocr = lire_fichier(ocrfile)
+                # print(texte[start_char:end_char])
+                # doc = nlp(texte[start_char:end_char])
+                doc_ocr = nlp(texte_ocr)
+                # ##Token Segmentation
+                dicocr_tok = tokenisation(doc_ocr, dicocr_tok, oc)
+                # ##Sentence Segmentation
+                dicocr_sent = sent_seg(doc_ocr, dicocr_sent, ic)
 
-
-##Segmentation
-
-                        doc = nlp(texte)
-                        assert doc.has_annotation("SENT_START")
-                        for sent in doc.sents:
-                            dico_res["segment" + str(i)] = sent.text
-                            i=i+1
-                            # print(i,"******",sent.text)
-                    stocker(f"{reffile}_seg_{mod}.json",dico_res)
-
-
-##REN
-        # print(path_file)
-        # if "OCR" in path_file:
-        #     ocr_files= f"{path_file}/*/*.txt"
-        #     for ocrfile in glob.glob(ocr_files):
-        #         print("OCR",ocrfile)
-        #         texte = lire_fichier(ocrfile)
-        #         print(texte[238567:238599])
-
-
-        # if "REF" in path_file:
-        #     ref_files= f"{path_file}/*.txt"
-        #     for reffile in glob.glob(ref_files):
-        #         print("REF", reffile)
-        #
+                stocker(f"{ocrfile}_tok_{mod}.json", dicocr_tok)
+                stocker(f"{ocrfile}_seg_{mod}.json", dicocr_sent)
