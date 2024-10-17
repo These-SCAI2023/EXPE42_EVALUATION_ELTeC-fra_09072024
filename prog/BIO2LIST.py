085db0302f4161687b7d8769bbe50c33f2ee6f03
These-SCAI2023 <caroline.parfait@sorbonne-universite.fr>
2024-10-17 22:03:10 +0200

    prog BIO2list

--- prog/BIO2LIST.py
+++ prog/BIO2LIST.py
@@ -8,7 +8,7 @@ def lire_csv(chemin):
         next(spamreader)
         for row in spamreader:
             # print(row)
-            if row[1]!="O":
+            if "LOC" in row[1]:
                 liste_row.append(row)
         return liste_row
 
@@ -32,23 +32,32 @@ for file_data in glob.glob(path_data):
     liste_entite = []
     premier=0
     for d in data:
-        if "B-" in d[1]:
-            if premier==0:
-                liste_mot.append(d[0])
-                liste_entite.append(d[1])
-                premier=1
-            else:
+        if d == data[-1]:
+            if "B-" in d[1]:
                 liste_toto.append(" ".join(liste_mot))
-                liste_mot = []
-                liste_entite = []
+                liste_toto.append(d[0])
+            if "I-" in d[1]:
+                liste_mot.append(d[0])
+                liste_toto.append(" ".join(liste_mot))
+        else:
+            if "B-" in d[1]:
+                if premier==0:
+                    liste_mot.append(d[0])
+                    liste_entite.append(d[1])
+                    premier=1
+                else:
+                    liste_toto.append(" ".join(liste_mot))
+                    liste_mot = []
+                    liste_entite = []
+                    liste_mot.append(d[0])
+                    liste_entite.append(d[1])
+
+            if "I-" in d[1]:
                 liste_mot.append(d[0])
                 liste_entite.append(d[1])
-
-        if "I-" in d[1]:
-            liste_mot.append(d[0])
-            liste_entite.append(d[1])
 
     print(liste_toto)
+    stocker(f"{file_data}-liste.json", liste_toto)
 
 
     # for data[i] in data:
