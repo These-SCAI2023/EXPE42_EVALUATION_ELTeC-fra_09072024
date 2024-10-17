e3de58aabb5c7648ca9edb582fe48c6cac0d18b9
These-SCAI2023 <caroline.parfait@sorbonne-universite.fr>
2024-10-17 21:40:20 +0200

    prog BIO2list

--- prog/BIO2LIST.py
+++ prog/BIO2LIST.py
@@ -27,32 +27,55 @@ for file_data in glob.glob(path_data):
     data = lire_csv(file_data)
     # print(data)
     i = 0
-    liste_mot= []
-    for data[i] in data:
-        # print(data[i])
-        if "I-" in data[i][1]:
-            # print(data[i-1])
-            if "I-" in data[i-1][1]:
-                dataconcat = data[i - 1][0] +" " +data[i][0]
-                # print(dataconcat)
-                if "I-" in data[i-2][1]:
-                    # print(data[i-2])
-                    dataconcat2 = data[i-2][0] +" " +dataconcat
-                    # print(detaconcat2)
+    liste_toto = []
+    liste_mot = []
+    liste_entite = []
+    premier=0
+    for d in data:
+        if "B-" in d[1]:
+            if premier==0:
+                liste_mot.append(d[0])
+                liste_entite.append(d[1])
+                premier=1
+            else:
+                liste_toto.append(" ".join(liste_mot))
+                liste_mot = []
+                liste_entite = []
+                liste_mot.append(d[0])
+                liste_entite.append(d[1])
 
-                if "B-" in data[i-2][1]:
-                    # print(data[i-2])
-                    dataconcat2 = data[i-2][0] + " "+dataconcat
-                    # print(detaconcat3)
-                    liste_mot.append(dataconcat2)
-        if "B-" in data[i][1]:
-            liste_mot.append(data[i][0])
+        if "I-" in d[1]:
+            liste_mot.append(d[0])
+            liste_entite.append(d[1])
 
-        i = i+1
-    print(liste_mot)
-    # stocker(f"{file_data}-liste.json")
-#     for data[i] in data:
-#         if "I-" in data[i][1]:
+    print(liste_toto)
+
+
+    # for data[i] in data:
+    #     # print(data[i])
+    #     if "I-" in data[i][1]:
+    #         # print(data[i-1])
+    #         if "I-" in data[i-1][1]:
+    #             dataconcat = data[i - 1][0] +" " +data[i][0]
+    #             # print(dataconcat)
+    #             if "I-" in data[i-2][1]:
+    #                 # print(data[i-2])
+    #                 dataconcat2 = data[i-2][0] +" " +dataconcat
+    #                 # print(detaconcat2)
+    #
+    #             if "B-" in data[i-2][1]:
+    #                 # print(data[i-2])
+    #                 dataconcat2 = data[i-2][0] + " "+dataconcat
+    #                 # print(detaconcat3)
+    #                 liste_mot.append(dataconcat2)
+    #     if "B-" in data[i][1]:
+    #         liste_mot.append(data[i][0])
+    #
+    #     i = i+1
+    # print(liste_mot)
+#     # stocker(f"{file_data}-liste.json")
+# #     for data[i] in data:
+# #         if "I-" in data[i][1]:
 #             # print(data[i-1])
 #             if "I-" in data[i-1][1]:
 #                 dataconcat = data[i - 1] + data[i]
