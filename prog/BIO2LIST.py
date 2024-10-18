c83a1aa9d77b4af1a811239dae2928ed8dd51df8
These-SCAI2023 <caroline.parfait@sorbonne-universite.fr>
2024-10-18 18:43:56 +0200

    prog BIO2list

--- prog/BIO2LIST.py
+++ prog/BIO2LIST.py
@@ -1,6 +1,8 @@
 import csv
 import glob
 import json
+
+
 def lire_csv(chemin):
     liste_row = []
     with open(chemin, newline='', encoding="utf-8") as csvfile:
@@ -8,9 +10,10 @@ def lire_csv(chemin):
         next(spamreader)
         for row in spamreader:
             # print(row)
-            if "LOC" in row[1]:
+            if "B-" in row[1]:
                 liste_row.append(row)
         return liste_row
+
 
 def stocker(chemin, contenu):
     w = open(chemin, "w")
@@ -21,78 +24,62 @@ def stocker(chemin, contenu):
 
 
 path_data = "../EXPE52_COMPARAISONS-ANNOTATIONS-MANUELLES_en cours/CORPUS_COMPAR/*/*TAL-ENS*/NER/*.bio"
-
+dico = {}
 for file_data in glob.glob(path_data):
     print(file_data)
     data = lire_csv(file_data)
+    liste_loc = []
+    liste_per = []
+    liste_org = []
+    liste_misc = []
+    name_file = file_data.split("/")[-1]
+    dico[name_file] = {}
     # print(data)
-    i = 0
-    liste_toto = []
-    liste_mot = []
-    liste_entite = []
-    premier=0
+    # liste_toto = []
+    # liste_mot = []
+    # liste_entite = []
+    # premier = 0
     for d in data:
-        if d == data[-1]:
-            if "B-" in d[1]:
-                liste_toto.append(" ".join(liste_mot))
-                liste_toto.append(d[0])
-            if "I-" in d[1]:
-                liste_mot.append(d[0])
-                liste_toto.append(" ".join(liste_mot))
-        else:
-            if "B-" in d[1]:
-                if premier==0:
-                    liste_mot.append(d[0])
-                    liste_entite.append(d[1])
-                    premier=1
-                else:
-                    liste_toto.append(" ".join(liste_mot))
-                    liste_mot = []
-                    liste_entite = []
-                    liste_mot.append(d[0])
-                    liste_entite.append(d[1])
+        # print(d)
+        if d[1] == "B-LOC":
+            liste_loc.append(d[0])
+            dico[name_file][d[1]] = len(liste_loc)
+        if d[1] == "B-PER":
+            liste_per.append(d)
+            dico[name_file][d[1]] = len(liste_per)
+        if d[1] == "B-ORG":
+            liste_org.append(d)
+            dico[name_file][d[1]] = len(liste_org)
 
-            if "I-" in d[1]:
-                liste_mot.append(d[0])
-                liste_entite.append(d[1])
+        if d[1] == "B-MISC":
+            liste_misc.append(d)
+            dico[name_file][d[1]] = len(liste_misc)
+    print(dico)
+    stocker("../EXPE52_COMPARAISONS-ANNOTATIONS-MANUELLES_en cours/CORPUS_COMPAR/dico_nombre.json", dico)
 
-    print(liste_toto)
-    stocker(f"{file_data}-liste.json", liste_toto)
-
-
-    # for data[i] in data:
-    #     # print(data[i])
-    #     if "I-" in data[i][1]:
-    #         # print(data[i-1])
-    #         if "I-" in data[i-1][1]:
-    #             dataconcat = data[i - 1][0] +" " +data[i][0]
-    #             # print(dataconcat)
-    #             if "I-" in data[i-2][1]:
-    #                 # print(data[i-2])
-    #                 dataconcat2 = data[i-2][0] +" " +dataconcat
-    #                 # print(detaconcat2)
+    #     if d == data[-1]:
+    #         if "B-" in d[1]:
+    #             liste_toto.append(" ".join(liste_mot))
+    #             liste_toto.append(d[0])
+    #         if "I-" in d[1]:
+    #             liste_mot.append(d[0])
+    #             liste_toto.append(" ".join(liste_mot))
+    #     else:
+    #         if "B-" in d[1]:
+    #             if premier == 0:
+    #                 liste_mot.append(d[0])
+    #                 liste_entite.append(d[1])
+    #                 premier = 1
+    #             else:
+    #                 liste_toto.append(" ".join(liste_mot))
+    #                 liste_mot = []
+    #                 liste_entite = []
+    #                 liste_mot.append(d[0])
+    #                 liste_entite.append(d[1])
     #
-    #             if "B-" in data[i-2][1]:
-    #                 # print(data[i-2])
-    #                 dataconcat2 = data[i-2][0] + " "+dataconcat
-    #                 # print(detaconcat3)
-    #                 liste_mot.append(dataconcat2)
-    #     if "B-" in data[i][1]:
-    #         liste_mot.append(data[i][0])
+    #         if "I-" in d[1]:
+    #             liste_mot.append(d[0])
+    #             liste_entite.append(d[1])
     #
-    #     i = i+1
-    # print(liste_mot)
-#     # stocker(f"{file_data}-liste.json")
-# #     for data[i] in data:
-# #         if "I-" in data[i][1]:
-#             # print(data[i-1])
-#             if "I-" in data[i-1][1]:
-#                 dataconcat = data[i - 1] + data[i]
-#                 if "B-" in data[i-2][1]:
-#                     # print(data[i-2])
-#                     detaconcat2 = data[i-2] + dataconcat
-#                     print(detaconcat2)
-#         i=i+1
-#
-#
-# print(liste_mot)
+    # print(liste_toto)
+    # stocker(f"{file_data}-liste.json", liste_toto)
